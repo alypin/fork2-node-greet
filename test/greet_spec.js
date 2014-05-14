0d0bb9984d6d380c4df076cfe6c04f123956309f
Jiahui Hu <jiahui.cs@gmail.com>
2014-05-14 08:59:56 -0400

    finish warmup2

--- test/greet_spec.js
+++ test/greet_spec.js
@@ -1,8 +1,8 @@
-var greet = require("greet");
+var greet = require("../index");
 
 describe('greet', function() {
   it("should greet a person by name", function() {
-    expect(greet('name')).to.eql("hello, " + name);
+    expect(greet('name')).to.eql("hello, name");
   });
   it("shoud greet a person by flirtatiously if drunk", function() {
     expect(greet("jiahui", true)).to.eql("hello jiahui, you look sexy today");
