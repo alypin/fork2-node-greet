8b57114b0a8f29d13e5fd8df6ff8e0ee1cf5c2f3
Jiahui Hu <jiahui.cs@gmail.com>
2014-05-14 15:43:36 -0400

    Convert index.js

--- bin/greet.js
+++ bin/greet.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-var greet = require("../index.js");
+var greet = require("../lib/index");
 args = require('minimist')(process.argv.slice(2));
 console.log(greet(args._, args.drunk))
